3fc33636fc72f1343b5cdd6aa630b170741f355e
TXin96 <609012488@qq.com>
2017-01-01 23:10:22 +0800

    Update ServerRunner_Driver.java

--- 336Server/src/main/java/runner/ServerRunner_Driver.java
+++ 336Server/src/main/java/runner/ServerRunner_Driver.java
@@ -2,12 +2,7 @@ package runner;
 
 import java.time.LocalDate;
 import java.util.List;
-<<<<<<< HEAD
-=======
 
-
-
->>>>>>> refs/remotes/origin/master
 import businesslogic.customerbl.CustomerBLService_Stub;
 import businesslogic.hotelbl.HotelBLService_Stub;
 import businesslogic.orderbl.OrderBLService_Stub;
